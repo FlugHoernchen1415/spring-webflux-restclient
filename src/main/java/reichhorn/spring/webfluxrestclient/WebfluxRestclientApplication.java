e4dbe331183d42c74cf327a87af11cf24ccb232d
Rene <flughoernchen1415@gmail.com>
2018-06-28 16:23:59 +0200

    initial commit

---
+++ src/main/java/reichhorn/spring/webfluxrestclient/WebfluxRestclientApplication.java
@@ -0,0 +1,12 @@
+package reichhorn.spring.webfluxrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxRestclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxRestclientApplication.class, args);
+    }
+}
